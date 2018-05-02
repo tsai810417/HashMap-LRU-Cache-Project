9d80e36a34a366a8a1b0d24ed900c4a4c09a8f48
tsai810417 <tsai_cheng@yahoo.com>
2018-05-02 02:08:10 -0700

    finish hash set

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,32 @@ class HashSet
   end
 
   def insert(key)
+    num = key.hash
+    num *= -1 if num < 0
+    while @count == num_buckets
+      resize!
+    end
+    @store[num % num_buckets].push(key)
+    @count += 1
   end
 
   def include?(key)
+    num = key.hash
+    num *= -1 if num < 0
+    @store[num % num_buckets].include?(key)
   end
 
   def remove(key)
+    num = key.hash
+    @store[num % num_buckets].delete(key)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 
   def num_buckets
@@ -28,5 +42,6 @@ class HashSet
   end
 
   def resize!
+    @store = @store.concat(Array.new(num_buckets) { Array.new })
   end
 end
